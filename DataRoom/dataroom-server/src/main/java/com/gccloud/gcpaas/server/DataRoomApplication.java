4c0534ec71929cc95934903aaf3e0fa133b436ec
liu.chengbiao <1537130693@qq.com>
2025-12-26 22:39:15 +0800

    chore(server): 初始化数据库连接及配置基础资源文件
    - 新增application.yml配置数据源及服务器端口信息
    - 添加banner.txt文件显示启动图案和项目链接
    - 在DataRoomApplication中注入JdbcTemplate并执行简单查询初始化连接
    - 更新pom.xml添加mysql-connector-j依赖
    - 修改.gitignore，排除application-dev.yml和spy.log日志文件

--- DataRoom/dataroom-server/src/main/java/com/gccloud/gcpaas/server/DataRoomApplication.java
+++ DataRoom/dataroom-server/src/main/java/com/gccloud/gcpaas/server/DataRoomApplication.java
@@ -1,10 +1,21 @@
 package com.gccloud.gcpaas.server;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.Resource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 public class DataRoomApplication {
+    @Resource
+    private JdbcTemplate jdbcTemplate;
+
+    @PostConstruct
+    public void init() {
+        jdbcTemplate.execute("select 1");
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(DataRoomApplication.class, args);
     }
