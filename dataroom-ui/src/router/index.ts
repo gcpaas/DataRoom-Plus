cebd65be7c607ee78d1bd635f6b147a3326a7446
liu.chengbiao <1537130693@qq.com>
2026-01-04 20:28:09 +0800

    chore(router): 添加版本号和项目Star提醒
    - 从package.json中引入版本号信息
    - 在控制台打印DataRoom-Plus版本号和Star链接
    - 更新package.json版本号为1.0.0

--- dataroom-ui/src/router/index.ts
+++ dataroom-ui/src/router/index.ts
@@ -1,4 +1,5 @@
 import {createRouter, createWebHashHistory} from 'vue-router'
+import config from '../../package.json'
 
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
@@ -58,4 +59,12 @@ const router = createRouter({
   ],
 })
 
+console.log(
+  '%cDataRoom-Plus%cv%s%c 请给我一个Star %s',
+  `font-size:24px;color:#3478f6;vertical-align: bottom;background:#ecf2fd;padding:0 10px;border-radius:8px;`,
+  `font-size:18px;color:#666;vertical-align: bottom;margin-left:12px;`,
+  config.version,
+  `font-size:18px;color:#999;vertical-align: bottom;margin-left:15px;text-decoration: none;`,
+  `https://gitee.com/gcpaas/DataRoom-Plus`
+);
 export default router
