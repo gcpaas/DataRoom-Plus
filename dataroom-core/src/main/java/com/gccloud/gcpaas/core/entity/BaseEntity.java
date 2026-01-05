ab1d333f993804a44cf8eb516f81dd7515b11c23
liu.chengbiao <1537130693@qq.com>
2026-01-05 16:40:14 +0800

    feat(core): 实现实体字段自动填充功能
    - 为BaseEntity类的创建时间、创建人、更新时间、更新人、租户编码字段增加自动填充注解
    - 引入@TableLogic注解实现逻辑删除处理
    - 新增MybatisMetaObjectHandler类实现MetaObjectHandler接口
    - 在插入操作中自动填充上述字段的默认值，包括当前用户和当前时间
    - 在更新操作中自动更新更新时间和更新人信息
    - 兼容无用户登录时的默认字段值填充逻辑

--- dataroom-core/src/main/java/com/gccloud/gcpaas/core/entity/BaseEntity.java
+++ dataroom-core/src/main/java/com/gccloud/gcpaas/core/entity/BaseEntity.java
@@ -20,32 +20,37 @@ public class BaseEntity {
     /**
      * 创建时间
      */
+    @TableField(fill = FieldFill.INSERT, updateStrategy = FieldStrategy.NEVER)
     @Schema(description = "创建时间")
     private Date createDate;
     /**
      * 创建人
      */
+    @TableField(fill = FieldFill.INSERT, updateStrategy = FieldStrategy.NEVER)
     @Schema(description = "创建人")
     private String createUser;
     /**
      * 更新时间
      */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
     @Schema(description = "更新时间")
     private Date updateDate;
     /**
      * 更新人
      */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
     @Schema(description = "更新人")
     private String updateUser;
     /**
      * 租户编码
      */
+    @TableField(fill = FieldFill.INSERT, updateStrategy = FieldStrategy.NEVER)
     @Schema(description = "租户编码")
     private String tenantCode;
     /**
      * 逻辑删除
      */
-    @Schema(description = "删除标识(0：正常，1：删除)",hidden = true)
+    @Schema(description = "删除标识(0：正常，1：删除)", hidden = true)
     @TableLogic(delval = "1", value = "0")
     private String delFlag = "0";
 }
